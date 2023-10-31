89805348576867c7297897f28b93fd3cdbf16787
Dmytro <predchishindmytro@gmail.com>
2023-10-31 13:20:33 +0200

    Fix security warning

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  distDir: 'out',
   output: 'export',
-  images: {
-    unoptimized: true
-  }
 }
 
 module.exports = nextConfig
